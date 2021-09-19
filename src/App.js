9709565dfcaccfa0d020052509226c60d0da3d3a
Niels Koster <n.koster@portavita.eu>
2021-09-19 23:07:12 +0200

    Add Header and Main components, and the Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import { HeaderProvider } from './context/Header'
 
 const App = () => {
   return (
     <div className='App'>
-      <h3>App</h3>
-      <img src='../assets/blacksquare.svg' />
+      <HeaderProvider>
+        <Header />
+        <Main />
+      </HeaderProvider>
     </div>
   )
 }
